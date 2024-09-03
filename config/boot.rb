623e79d5421dd7644973ac27dc3980d4471343c0
weeezik <mkihanya.33@gmail.com>
2024-09-03 12:43:12 -0700

    Remove code that was added to config/boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -3,11 +3,11 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 
-module Rails  
-  class Server
-    alias :default_options_bk :default_options
-    def default_options
-      default_options_bk.merge!(Host: '0.0.0.0')
-    end
-  end
-end
+# module Rails  
+#   class Server
+#     alias :default_options_bk :default_options
+#     def default_options
+#       default_options_bk.merge!(Host: '0.0.0.0')
+#     end
+#   end
+# end
